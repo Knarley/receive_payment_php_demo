2d1c34f410af4e6c18e1e051da6b0545c665ed01
Knarley <knarleyhf@gmail.com>
2013-07-06 21:23:54 -0400

    Update callback.php
    mysql_* functions are deprecated.  Using something like PDO (Which I used) or mysqli is much more secure and it will be in future updates of php.  What I did should work, hopefully most of this git will change.  Thanks.

--- callback.php
+++ callback.php
@@ -4,7 +4,7 @@ include 'include.php';
 
 mysql_connect($mysql_host, $mysql_username, $mysql_password) or die(__LINE__ . ' Invalid connect: ' . mysql_error());
 
-mysql_select_db($mysql_database) or die( "Unable to select database. Run setup first.");
+$odb = new PDO('mysql:host=' .$mysql_host. ';dbname='.$mysql_db, $mysql_username, $mysql_password);
 
 $invoice_id = $_GET['invoice_id'];
 $transaction_hash = $_GET['transaction_hash'];
@@ -28,7 +28,9 @@ if ($_GET['secret'] != $secret) {
 
 if ($_GET['confirmations'] >= 4) {
   //Add the invoice to the database
-  $result = mysql_query("replace INTO invoice_payments (invoice_id, transaction_hash, value) values($invoice_id, '$transaction_hash', $value_in_btc)");
+  $replaceQuery = $odb->prepare("REPLACE INTO `invoice_payments`  VALUES (:invoice_id, :transaction_hash, :value_in_btc)");
+  $replaceQuery->execute(array(':invoice_id' => $invoice_id, ':transaction_hash' => $transaction_hash, ':value_in_btc' => $value_in_btc));
+  $result = $replaceQuery->fetch(PDO::FETCH_ASSOC); 
 
   //Delete from pending
   mysql_query("delete from pending_invoice_payments where invoice_id = $invoice_id limit 1");
